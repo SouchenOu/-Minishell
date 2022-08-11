ffeccf80a5b6945bacf0a6c52a49190f644ad2ed
Soukaina Ouchen <souchen@e3r5p5.1337.ma>
2022-08-11 17:22:21 +0100

    minishell final

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,36 +6,45 @@
 /*   By: souchen <souchen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 11:35:52 by souchen           #+#    #+#             */
-/*   Updated: 2022/08/11 15:45:27 by souchen          ###   ########.fr       */
+/*   Updated: 2022/08/11 17:20:55 by souchen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_join(const char *s1, const char *s2)
 {
-	char	*test;
+	char	*join;
+	int		i;
 	int		j;
 
+	i = 0;
 	j = 0;
-	if (s1 == NULL)
-	{
+	join = malloc((sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1)));
+	if (!(join))
 		return (NULL);
-	}
-	test = (char *)malloc (sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (test == NULL)
+	while (s1[i])
 	{
-		return (NULL);
-	}
-	while (*s1 != '\0')
-	{
-		test[j] = *(s1++);
+		join[j] = s1[i];
+		i++;
 		j++;
 	}
-	while (*s2 != '\0')
+	i = 0;
+	while (s2[i])
 	{
-		test[j++] = *(s2++);
+		join[j] = s2[i];
+		j++;
+		i++;
 	}
-	test[j] = '\0';
-	return (test);
+	join[j] = '\0';
+	return (join);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	if (s1 == NULL)
+		return (ft_strdup(s2));
+	if (s2 == NULL)
+		return (ft_strdup(s1));
+	return (ft_join(s1, s2));
 }
